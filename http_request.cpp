db262536f788077d620482014b0bfeaabaaa3517
kingstonv1 <kingstongise@gmail.com>
2025-02-25 14:53:14 -0500

    Request header parsing!

--- http_request.cpp
+++ http_request.cpp
@@ -2,6 +2,7 @@
 
 #include <map>
 #include <string>
+#include <sstream>
 #include <vector>
 
 void http_request::parse_request() {
@@ -46,7 +47,33 @@ void http_request::parse_request() {
 }
 
 void http_request::parse_headers() {
-    return;
+    std::istringstream stream(this->buffer_string);
+    std::string ln;
+
+    std::getline(stream, ln, '\r'); // the first (req) line is ignored
+    stream.ignore(1); // ignore the \n after \r in http requests
+
+    while (std::getline(stream, ln, '\r')) {
+        stream.ignore(1);
+
+        if (ln.empty()) continue;
+
+        size_t colon = ln.find(':');
+
+        if (colon == std::string::npos) {
+            continue;
+        }
+
+        std::string key = ln.substr(0, colon); 
+        std::string val = ln.substr(colon + 1);
+
+        // Trim whitespace from value
+        size_t start = val.find_first_not_of(" \t");
+        size_t end = val.find_last_not_of(" \t");
+        val = (start == std::string::npos) ? "" : val.substr(start, end - start + 1);
+
+        this->headers[key] = val;
+    }
 }
 
 http_request::http_request(const char* buf) {
@@ -70,6 +97,7 @@ std::string http_request::get_method() {
 
     return "UNDEF";
 }
+
 std::string http_request::get_path() { return resource_path; }
 std::string http_request::get_version() { return http_version; }
 std::string http_request::get_reqstr() { return request_string; }
